77dfaa1ff846f711a0bd1eaf20c1acd669c1fbf4
mulqi <mulkoi92@gmail.com>
2025-08-24 22:04:47 +0700

    kkkkkk

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const fs = require("fs");
-const followers = require("./followers_1.json");
-const following = require("./following.json");
+const fs = require('fs')
+const followers = require('./followers_1.json')
+const following = require('./following.json')
 
 const main = () => {
   const yourFollowers = followers.map(item => item.string_list_data[0].value)
-  const yourFollowing = following.relationships_following.map(item => item.string_list_data[0].value)
+  const yourFollowing = following.relationships_following.map(
+    item => item.string_list_data[0].value
+  )
 
-  const notFollowingBack = yourFollowing.filter(user => !yourFollowers.includes(user));
+  const notFollowingBack = yourFollowing.filter(
+    user => !yourFollowers.includes(user)
+  )
 
-  fs.writeFileSync("output.txt", JSON.stringify(notFollowingBack, null, 2), "utf-8");
+  fs.writeFileSync(
+    'notFollowback.txt',
+    JSON.stringify(notFollowingBack, null, 2),
+    'utf-8'
+  )
 }
- 
- 
 
-
-main();
+main()
